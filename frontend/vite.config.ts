bac38e30316b020d08fc9318e14278d3536b3dc4
DuyTek <100891449+DuyTek@users.noreply.github.com>
2023-09-29 13:19:11 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+// This is Vite config
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
