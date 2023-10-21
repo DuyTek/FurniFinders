cabeba8b92541ce159a6f8f7241cbd99d053a3eb
Duy Nguyen Dang Vu <duy.dnguyen@katalon.com>
2023-10-21 09:23:29 +0700

    Clone source minimal kit

---
+++ frontend/src/layouts/dashboard/config-navigation.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import SvgColor from '../../components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => (
+  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
+const navConfig = [
+  {
+    title: 'posts',
+    path: '/blog',
+    icon: icon('ic_blog'),
+  },
+  {
+    title: 'dashboard',
+    path: '/',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'user',
+    path: '/user',
+    icon: icon('ic_user'),
+  },
+  {
+    title: 'product',
+    path: '/products',
+    icon: icon('ic_cart'),
+  },
+];
+
+export default navConfig;
