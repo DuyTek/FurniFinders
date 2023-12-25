bd69bc00904eb957902dfef3e9a59edfb29b7fd3
DuyTek <100891449+DuyTek@users.noreply.github.com>
2023-12-26 01:08:51 +0700

    [EF-28] User can update their personal profile (#24)
    Update user profile

--- backend/src/main/java/com/furnifinders/backend/Controller/UserController.java
+++ backend/src/main/java/com/furnifinders/backend/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.furnifinders.backend.Controller;
 
+import com.furnifinders.backend.Entity.User;
 import com.furnifinders.backend.dto.Request.EditProfileRequest;
 import com.furnifinders.backend.dto.Response.EditProfileResponse;
 import com.furnifinders.backend.service.UserService;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3030")
 public class UserController {
     private final UserService userService;
 
@@ -24,5 +26,11 @@ public class UserController {
             editProfileResponse.setMessage(e.getMessage());
         }
         return ResponseEntity.ok().body(editProfileResponse);
+    }
+
+    @GetMapping("/getProfile/{id}")
+    public ResponseEntity<User> getProfile (@PathVariable("id") Long id) {
+        User user = userService.findUserById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
